91673e18e7c43a15aa520b664c90a718745b7e9f
BayLak-ONE <mh1231231234go@gmail.com>
2025-01-04 03:10:11 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-
-# عنوان المستودع على GitHub
-REPO_URL="https://github.com/BayLak-ONE/servers-open-port-tester-metasploit.git"
-
-# الحصول على المسار الحالي للمجلد الذي يتم تشغيل السكربت فيه
+REPO_URL="https://github.com/BayLak-ONE/Snake_Engine.git"
 CURRENT_DIR=$(pwd)
-
-# تحقق إذا كان المجلد يحتوي بالفعل على مستودع Git
 if [ -d ".git" ]; then
-    echo "المستودع موجود بالفعل. سيتم سحب التحديثات بدلاً من تحميل المستودع من جديد."
     git pull origin main
 else
-    echo "تهيئة المستودع في المجلد الحالي..."
-    git init  # تهيئة مستودع Git جديد
-    git remote add origin "$REPO_URL"  # إضافة المستودع البعيد
-    git fetch origin  # جلب البيانات من المستودع البعيد
-    git reset --hard origin/main  # استعادة التحديثات من الفرع الرئيسي
+    git init
+    git remote add origin "$REPO_URL
+    git fetch origi
+    git reset --hard origin/main
 fi
-
-echo "تم تحميل المستودع أو تحديثه بنجاح!"
